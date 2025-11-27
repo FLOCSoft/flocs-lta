f2baf3d905c21bd4ad1a501d32b8442b2256da61
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-11-27 17:30:49 +0000

    Add automatic untar and verification to download

--- flocs_lta/flocs_download.py
+++ flocs_lta/flocs_download.py
@@ -5,8 +5,8 @@ from typing import Iterable
 import typer
 from enum import Enum
 from stager_access import get_macaroons, get_webdav_urls_requested
-from typer import Argument, Option
-from typing_extensions import Annotated
+from typer import Argument, Option, Optional
+from typing_extensions import Annotated, Literal
 
 # app = typer.Typer(add_completion=False)
 
@@ -27,17 +27,18 @@ class Downloader:
         self.macaroons = macaroons
         self.urls = urls
 
-    def download_url(self, url: str):
+    def download_url(self, url: str, outdir: str = "", extract: bool = False, verification: str = "basic"):
         """Download the MS pointed to by the URL.
 
         Args:
             url (str): URL to download.
+            outdir (str): directory to put downloaded files in.
 
         Raises:
             RuntimeError: when encountering an unknown LTA site.
         """
         site = None
-        outname = url.split("/")[-1]
+        outname = os.path.join(os.path.abspath(outdir), url.split("/")[-1])
         if "juelich" in url:
             site = LTASite.JUELICH
         if "psnc" in url:
@@ -50,15 +51,30 @@ class Downloader:
         os.system(
             f"wget --no-clobber --retry-on-http-error 401,500 --check-certificate=off {url}?authz={self.macaroons[site.value]} -O {outname}"
         )
+        if extract:
+            import tarfile
 
-    def download_all(self, max_workers: int):
+            with tarfile.open(outname, "r") as tarball:
+                tarball.extractall(path=outdir)
+
+            if verification == "basic":
+                import casacore.tables as ct
+                ms = outname.split("MS")[0]+"MS"
+                try:
+                    # strip the hash + tar extension
+                    ct.table(ms)
+                    os.remove(outname)
+                except:
+                    print(f"{ms} is not a valid MeasurementSet")
+
+    def download_all(self, max_workers: int, extract: bool = False, verification: str = "basic"):
         """Download all URLs belonging to the instance.
 
         Args:
             max_workers (int):
         """
         with ProcessPoolExecutor(max_workers=max_workers) as pex:
-            pex.map(self.download_url, self.urls)
+            pex.map(self.download_url, self.urls, extract=extract, verification=verification)
 
 
 def download(
@@ -66,12 +82,18 @@ def download(
     parallel_downloads: Annotated[
         int, Option(help="Maximum number of parallel downloads.")
     ] = 1,
+    extract: Annotated[
+        Optional[bool], Option(help="Extract the tarball after downloading.")
+    ] = True,
+    verification: Annotated[
+        Optional[Literal["basic"]], Option(help="Only used when `extract` is True. Sets the verification level to perform after extracting the tarball.")
+    ] = None,
 ):
     """Download data from the LTA that was staged via the StageIt service."""
     urls = get_webdav_urls_requested(stage_id)
     macaroons = get_macaroons(stage_id)
     dl = Downloader(urls, macaroons[0])
-    dl.download_all(parallel_downloads)
+    dl.download_all(parallel_downloads, extract=extract, verification=verification)
 
 
 def main():
